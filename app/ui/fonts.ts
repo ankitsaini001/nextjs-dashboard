1e77e136549161cafc75d15fdd7d1b331b5df4e2
ankitsaini001 <ankitsaini594@gmail.com>
2026-02-07 15:25:48 +0530

    initial commit

---
+++ app/ui/fonts.ts
@@ -0,0 +1,10 @@
+import {Inter, Lusitana} from 'next/font/google';
+
+export const InterFont = Inter({
+    subsets:['latin'],
+});
+
+export const LusitanaFont = Lusitana({
+    subsets:['latin'],
+    weight:['400', '700'],
+});
